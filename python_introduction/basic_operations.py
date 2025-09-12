b17a9088c8c27541c3bb97111cdc27e17edd9467
MudMoh <mudesir@gmail.com>
2025-09-12 20:59:09 +0300

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+quotient = number1 / number2
+
+# [operation] of [number1] and [number2] is [result].
+
+print('Addition of', number1, 'and', number2, 'is', sum)
+print('Subtraction of', number1, 'and', number2, 'is', difference)
+print('Multiplication of', number1, 'and', number2, 'is', product)
+print('Quotient of', number1, 'and', number2, 'is', quotient)
